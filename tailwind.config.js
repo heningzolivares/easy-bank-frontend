615cb227553f186510fb2f814eba28d87b51befe
Heningz Olivares <heningzolivares.dev@gmail.com>
2022-07-22 19:38:01 -0400

    Added react query, axios and public font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["'Public Sans', sans-serif", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
