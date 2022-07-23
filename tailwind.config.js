9ff01ad9e8e59ed328b238a4ac4080b5a0b875d2
Heningz Olivares <heningzolivares.dev@gmail.com>
2022-07-22 20:02:19 -0400

    theming colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,15 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    color: {
+      darkblue: '#2D314D',
+      primary: '#2D314E',
+      label: '#2D314E',
+      muted: '#959499',
+      gray: '#99989D',
+      turquoise: '#31CF72',
+      litblue: '#2CBAC7',
+    },
     extend: {
       fontFamily: {
         sans: ["'Public Sans', sans-serif", ...defaultTheme.fontFamily.sans],
