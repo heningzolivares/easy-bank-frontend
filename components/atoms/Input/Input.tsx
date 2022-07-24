3ef3a3dae3c88b12ebb97357c12030c97ddada2a
Heningz Olivares <heningzolivares.dev@gmail.com>
2022-07-24 13:08:43 -0400

    Textarea atomic component

--- components/atoms/Input/Input.tsx
+++ components/atoms/Input/Input.tsx
@@ -13,8 +13,7 @@ const Input: FC<InputProps> = ({ label, id, type = 'text', ...rest }) => {
       <input
         type={type}
         id={id}
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5    "
-        placeholder="name@flowbite.com"
+        className="bg-white border border-gray-400 text-gray-900 text-sm rounded-md focus:ring-green-300 focus:border-green-300 block w-full p-2.5 mb-5 shadow-sm"
         {...rest}
       ></input>
     </>
--- components/molecules/AddArticleForm/AddArticleForm.tsx
+++ components/molecules/AddArticleForm/AddArticleForm.tsx
@@ -1,11 +1,27 @@
 import type { FC, ReactNode } from 'react';
 
+import Button from 'components/atoms/Button/Button';
 import Card from 'components/atoms/Card/Card';
+import Input from 'components/atoms/Input/Input';
+import Textarea from 'components/atoms/Textarea/Textarea';
 
-const AddArticleForm: FC = () => (
-  <div className="flex flex-col w-formwidth h-8">
-    <Card> goo</Card>
-  </div>
-);
+const AddArticleForm: FC = () => {
+  return (
+    <div className="flex flex-col w-formwidth">
+      <Card>
+        <form>
+          <div className="flex flex-col">
+            <Input label="Author" name="author" id="author" autoFocus />
+            <Input label="Blog Title" name="blogTitle" id="blogTitle" />
+            <Textarea label="Blog Content" name="blogContent" id="blogContent" />
+            <Button className="w-full" type="submit">
+              Save
+            </Button>
+          </div>
+        </form>
+      </Card>
+    </div>
+  );
+};
 
 export default AddArticleForm;
